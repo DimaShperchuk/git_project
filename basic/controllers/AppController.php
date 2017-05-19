1049d880a09176df4c1f538a025e975132f1ce6a
shperchuk <dimashperchuk@gmail.com>
2017-05-19 20:15:15 +0300

    Removed unnecessary from the address bar

---
+++ basic/controllers/AppController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shperchuk
+ * Date: 18.05.2017
+ * Time: 22:23
+ */
+
+namespace app\controllers;
+
+
+class AppController
+{
+
+}
---
+++ basic/controllers/PostController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shperchuk
+ * Date: 18.05.2017
+ * Time: 23:00
+ */
+
+namespace app\controllers;
+
+
+class PostController
+{
+
+}
